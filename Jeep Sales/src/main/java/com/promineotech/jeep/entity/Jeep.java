0dcce66449435fd12fa140f0f00fc2767f178625
Allen Bellinger <bellingerallen@gmail.com>
2022-03-16 13:14:03 -0500

    Added files for homework including updated Jeep Sales files and Word document with screenshots for homework

---
+++ Jeep Sales/src/main/java/com/promineotech/jeep/entity/Jeep.java
@@ -0,0 +1,18 @@
+package com.promineotech.jeep.entity;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Jeep {
+    private long modelPK;
+    private JeepModel modelId;
+    private String trimLevel;
+    private int numDoors;
+    private int wheelSize;
+    private BigDecimal basePrice;
+}
---
+++ Jeep Sales/src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -0,0 +1,45 @@
+package com.promineotech.jeep.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+import com.promineotech.jeep.support.FetchJeepTestSupport;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Sql(scripts = {
+        "classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
+        "classpath:flyway/migrations/V1.1__Jeep_Data.sql"},
+        config = @SqlConfig(encoding = "utf-8"))
+class FetchJeepTest extends FetchJeepTestSupport {
+
+    @Test
+    void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
+        // Given: a valid model, trim and URI
+        JeepModel model = JeepModel.WRANGLER;
+        String trim = "Sport";
+        String uri = String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
+
+        // When: a connection is made to the URI
+        ResponseEntity<List<Jeep>> response = getRestTemplate().exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+
+        // Then: a success-OK status code is returned
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        // And: the actual list returned is the same as the expected list
+        List<Jeep> expected = buildExpected();
+        System.out.println(expected);
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
+}
